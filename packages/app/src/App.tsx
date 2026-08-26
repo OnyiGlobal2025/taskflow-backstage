d3618219eaec58ba9b12738da80caf9d40e2b5cf
OnyiGlobal2025 <onyedikaokoro8@gmail.com>
2026-08-26 10:18:39 +0100

    feat: add mermaid addon for TechDocs diagram rendering

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,7 +1,9 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import techDocsPlugin from '@backstage/plugin-techdocs/alpha';
+import { techDocsMermaidAddonModule } from 'backstage-plugin-techdocs-addon-mermaid';
 import { navModule } from './modules/nav';
 
 export default createApp({
-  features: [catalogPlugin, navModule],
+  features: [catalogPlugin, techDocsPlugin, techDocsMermaidAddonModule, navModule],
 });
