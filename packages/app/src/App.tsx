23d8c2bf5ea5858a182ae41cc5b79ac824b0f5ca
OnyiGlobal2025 <onyedikaokoro8@gmail.com>
2026-09-02 16:31:30 +0100

    feat(phase-7): enable Kubernetes plugin and ArgoCD proxy integration

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -2,11 +2,14 @@ import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import techDocsPlugin from '@backstage/plugin-techdocs/alpha';
 import { techDocsMermaidAddonModule } from 'backstage-plugin-techdocs-addon-mermaid';
+import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
 import { navModule } from './modules/nav';
 import { githubAuthApiRef } from '@backstage/core-plugin-api';
 import { SignInPageBlueprint } from '@backstage/plugin-app-react';
 import { SignInPage } from '@backstage/core-components';
 import { createFrontendModule } from '@backstage/frontend-plugin-api';
+import { EntityCardBlueprint } from '@backstage/plugin-catalog-react/alpha';
+import { compatWrapper } from '@backstage/core-compat-api';
 
 const signInPage = SignInPageBlueprint.make({
   params: {
@@ -24,15 +27,42 @@ const signInPage = SignInPageBlueprint.make({
   },
 });
 
+const argoCdOverviewCard = EntityCardBlueprint.make({
+  name: 'argocd-overview',
+  params: {
+    filter: 'has:annotation:argocd/app-name',
+    loader: () =>
+      import('@roadiehq/backstage-plugin-argo-cd').then(m =>
+        compatWrapper(<m.EntityArgoCDOverviewCard />),
+      ),
+  },
+});
+
+const argoCdHistoryCard = EntityCardBlueprint.make({
+  name: 'argocd-history',
+  params: {
+    filter: 'has:annotation:argocd/app-name',
+    loader: () =>
+      import('@roadiehq/backstage-plugin-argo-cd').then(m =>
+        compatWrapper(<m.EntityArgoCDHistoryCard />),
+      ),
+  },
+});
+
 export default createApp({
   features: [
     catalogPlugin,
     techDocsPlugin,
     techDocsMermaidAddonModule,
+    kubernetesPlugin,
     navModule,
     createFrontendModule({
       pluginId: 'app',
       extensions: [signInPage],
+    }),
+    createFrontendModule({
+      pluginId: 'catalog',
+      extensions: [argoCdOverviewCard, argoCdHistoryCard],
     }),
   ],
 });
