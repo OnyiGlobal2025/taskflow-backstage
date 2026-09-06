2c002e12c21e0fc7f645fe4d3331a14f2f2f1eed
OnyiGlobal2025 <onyedikaokoro8@gmail.com>
2026-09-06 11:17:18 +0100

    fix(argocd): use plugin's native alpha registration, remove duplicate hand-registered cards

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -3,14 +3,12 @@ import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import techDocsPlugin from '@backstage/plugin-techdocs/alpha';
 import { techDocsMermaidAddonModule } from 'backstage-plugin-techdocs-addon-mermaid';
 import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
+import argocdPlugin from '@roadiehq/backstage-plugin-argo-cd/alpha';
 import { navModule } from './modules/nav';
 import { githubAuthApiRef } from '@backstage/core-plugin-api';
 import { SignInPageBlueprint } from '@backstage/plugin-app-react';
 import { SignInPage } from '@backstage/core-components';
 import { createFrontendModule } from '@backstage/frontend-plugin-api';
-import { EntityCardBlueprint } from '@backstage/plugin-catalog-react/alpha';
-import { compatWrapper } from '@backstage/core-compat-api';
-import { useEntity } from '@backstage/plugin-catalog-react';
 
 const signInPage = SignInPageBlueprint.make({
   params: {
@@ -28,48 +26,17 @@ const signInPage = SignInPageBlueprint.make({
   },
 });
 
-const argoCdOverviewCard = EntityCardBlueprint.make({
-  name: 'argocd-overview',
-  params: {
-    filter: 'has:annotation:argocd/app-name',
-    loader: async () => {
-      const { EntityArgoCDOverviewCard } = await import(
-        '@roadiehq/backstage-plugin-argo-cd'
-      );
-      const Wrapper = () => {
-        const { entity } = useEntity();
-        return compatWrapper(<EntityArgoCDOverviewCard entity={entity} />);
-      };
-      return <Wrapper />;
-    },
-  },
-});
-
-const argoCdHistoryCard = EntityCardBlueprint.make({
-  name: 'argocd-history',
-  params: {
-    filter: 'has:annotation:argocd/app-name',
-    loader: () =>
-      import('@roadiehq/backstage-plugin-argo-cd').then(m =>
-        compatWrapper(<m.EntityArgoCDHistoryCard />),
-      ),
-  },
-});
-
 export default createApp({
   features: [
     catalogPlugin,
     techDocsPlugin,
     techDocsMermaidAddonModule,
     kubernetesPlugin,
+    argocdPlugin,
     navModule,
     createFrontendModule({
       pluginId: 'app',
       extensions: [signInPage],
-    }),
-    createFrontendModule({
-      pluginId: 'catalog',
-      extensions: [argoCdOverviewCard, argoCdHistoryCard],
     }),
   ],
 });
